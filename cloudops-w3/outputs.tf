98b9eff8994ba64289ef8ba2e0d799b89d750813
Melvin C V <melrockz@gmail.com>
2023-08-23 11:27:17 +0530

    added outputs

---
+++ cloudops-w3/outputs.tf
@@ -0,0 +1,19 @@
+output "aurora_db_ep" {
+  value = aws_rds_cluster.cloudops-w3-db.endpoint
+}
+
+output "aurora_db_cluster_members" {
+  value = aws_rds_cluster.cloudops-w3-db.cluster_members
+}
+
+output "aurora_db_cluster_id" {
+  value = aws_rds_cluster.cloudops-w3-db.id
+}
+
+output "ec2_web1_publicip" {
+  value = aws_instance.cloudops-w3-web1.public_ip
+}
+
+output "ec2_web2_publicip" {
+  value = aws_instance.cloudops-w3-web2.public_ip
+}
